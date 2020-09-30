2044dcffcaaadba618e352a7dcc66fa8cd4ecef4
arvinrasheda <arvinrasheda121@gmail.com>
2020-09-30 11:41:10 +0700

    first commit

---
+++ conf/connection.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database_name = "workshop";
+mysqli_connect($host ,$user, $password, $database_name) or die('Connection to Database Failed !');
+?>
