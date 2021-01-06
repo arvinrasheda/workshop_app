2ae78f98984b36f33d106a891ee4a4c000ce49b7
arvinrasheda <arvinrasheda121@gmail.com>
2021-01-06 19:58:06 +0700

    update sql

--- sql/workshop.sql
+++ sql/workshop.sql
@@ -1,178 +1,103 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 06, 2021 at 01:50 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : Local
+ Source Server Type    : MySQL
+ Source Server Version : 100414
+ Source Host           : localhost:3306
+ Source Schema         : workshop
 
+ Target Server Type    : MySQL
+ Target Server Version : 100414
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 06/01/2021 19:57:50
+*/
 
---
--- Database: `workshop`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for materi
+-- ----------------------------
+DROP TABLE IF EXISTS `materi`;
+CREATE TABLE `materi`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `jenis` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `link` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `pelatihan_id` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
---
--- Table structure for table `materi`
---
+-- ----------------------------
+-- Records of materi
+-- ----------------------------
+INSERT INTO `materi` VALUES (1, 'nama', 'DOKUMEN', 'wallpaperflare.com_wallpaper.jpg$$', 'dwok$$', 2);
+INSERT INTO `materi` VALUES (2, '', 'DARING', 'https://www.youtube.com/$', 'deskripsi\r\n$', 0);
+INSERT INTO `materi` VALUES (8, 'nama aaa', 'DARING', 'https://www.youtube.com/$$$$$$$', 'aa$adwd$$', 2);
+INSERT INTO `materi` VALUES (9, 'bbbb', 'DARING', 'https://www.youtube.com/$$', 'dwd$$', 2);
 
-CREATE TABLE `materi` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `jenis` varchar(10) NOT NULL,
-  `link` text DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `pelatihan_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+-- ----------------------------
+-- Table structure for pelatihan
+-- ----------------------------
+DROP TABLE IF EXISTS `pelatihan`;
+CREATE TABLE `pelatihan`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `harga` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
---
--- Dumping data for table `materi`
---
+-- ----------------------------
+-- Records of pelatihan
+-- ----------------------------
+INSERT INTO `pelatihan` VALUES (2, 'Pelatihan 2', '100000');
 
-INSERT INTO `materi` (`id`, `nama`, `jenis`, `link`, `description`, `pelatihan_id`) VALUES
-(1, 'nama', 'DOKUMEN', 'wallpaperflare.com_wallpaper.jpg$$', 'dwok$$', 2),
-(2, '', 'DARING', 'https://www.youtube.com/$', 'deskripsi\r\n$', 0),
-(8, 'nama aaa', 'DARING', 'https://www.youtube.com/$$$$$$$', 'aa$adwd$$', 2),
-(9, 'bbbb', 'DARING', 'https://www.youtube.com/$$', 'dwd$$', 2);
+-- ----------------------------
+-- Table structure for peserta
+-- ----------------------------
+DROP TABLE IF EXISTS `peserta`;
+CREATE TABLE `peserta`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `pelatihan_id` int NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `nohp` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `order_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `filename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of peserta
+-- ----------------------------
+INSERT INTO `peserta` VALUES (4, 89, 2, 'mput@email.com', '91823123', 'INV7763348', 'DONE', 'INV7763348.docx');
 
---
--- Table structure for table `pelatihan`
---
-
-CREATE TABLE `pelatihan` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `harga` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `pelatihan`
---
-
-INSERT INTO `pelatihan` (`id`, `name`, `harga`) VALUES
-(2, 'Pelatihan 2', '100000');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `peserta`
---
-
-CREATE TABLE `peserta` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `pelatihan_id` int(11) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `nohp` varchar(255) DEFAULT NULL,
-  `order_id` varchar(10) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `filename` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `peserta`
---
-
-INSERT INTO `peserta` (`id`, `user_id`, `pelatihan_id`, `email`, `nohp`, `order_id`, `status`, `filename`) VALUES
-(4, 89, 2, 'mput@email.com', '91823123', 'INV7763348', 'DONE', 'INV7763348.docx');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
-  `is_active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 94 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
 
---
--- Dumping data for table `users`
---
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'Arvin', 'arvin', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1);
+INSERT INTO `users` VALUES (2, 'Eka', 'eka', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1);
+INSERT INTO `users` VALUES (3, 'Indra', 'indra', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1);
+INSERT INTO `users` VALUES (4, 'Ady', 'ady', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1);
+INSERT INTO `users` VALUES (5, 'Wahab', 'wahab', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1);
+INSERT INTO `users` VALUES (89, 'Mput', 'mput', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 0, 1);
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `is_admin`, `is_active`) VALUES
-(1, 'Arvin', 'arvin', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1),
-(2, 'Eka', 'eka', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1),
-(3, 'Indra', 'indra', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1),
-(4, 'Ady', 'ady', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1),
-(5, 'Wahab', 'wahab', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 1, 1),
-(89, 'Mput', 'mput', '$2y$10$LEZFoxQ1WytyYD.Wlc/PJ.cIgFPulKDbNkyCWYJjNOv/Aj/8ei4bu', 0, 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `materi`
---
-ALTER TABLE `materi`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Indexes for table `pelatihan`
---
-ALTER TABLE `pelatihan`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Indexes for table `peserta`
---
-ALTER TABLE `peserta`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `materi`
---
-ALTER TABLE `materi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `pelatihan`
---
-ALTER TABLE `pelatihan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `peserta`
---
-ALTER TABLE `peserta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
